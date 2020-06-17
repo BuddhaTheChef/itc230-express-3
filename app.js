9dfac47ac949a1e7944becb4474089d14cfc8800
Aj Wietecha II <ajwietechaii@yahoo.com>
2020-06-17 09:36:38 -0700

    Update app.js

--- app.js
+++ app.js
@@ -32,6 +32,12 @@ collection.find({}, function(err, docs){
     }
 });
 
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+
 
 
 app.get('/collections/:name',function(req,res){
@@ -43,11 +49,6 @@ app.get('/collections/:name',function(req,res){
 
 
 
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
